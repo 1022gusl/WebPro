3a29be5997ca7f2d8b3dec0fcff41fc3e478d21c
1022gusl <1022gusl@gmail.com>
2025-09-17 14:28:22 +0900

    feat: src4-3 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import Header from './components/Header';
-import Home from './components/Home';
-import ProductList from './components/ProductList';
-import Footer from './components/Footer';
-
-function App() {
+function UserInfo({ user }) {
+  const isMinor = user.age < 18;
   return (
-    <div className="container">
-      <Header />
-      <Home />
-      <ProductList />
-      <Footer />
+    <div className="m-4">
+      <h1 className="font-bold text-xl">문제 3) 사용자 정보</h1>
+      <div className="flex flex-col gap-2 mt-4">
+        <p>이름: {user.name}</p>
+        <p>나이: {user.age}</p>
+        {isMinor && <p>미성년자</p>}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  const user = { name: '홍길동', age: 17 };
+  return <UserInfo user={user} />;
+}
