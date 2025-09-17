df7ccd5010da4bdeff02c0e94c3ec42f3f0a5980
1022gusl <1022gusl@gmail.com>
2025-09-17 14:03:09 +0900

    feat: src4-1 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import Header from './components/Header';
-import Home from './components/Home';
-import ProductList from './components/ProductList';
-import Footer from './components/Footer';
-
-function App() {
+function ProductItem({ name, price }) {
   return (
-    <div className="container">
-      <Header />
-      <Home />
-      <ProductList />
-      <Footer />
+    <li>
+      {name} - {price.toLocaleString()}원
+    </li>
+  );
+}
+
+function ProductList({ products }) {
+  return (
+    <div className="mx-10 my-10">
+      <h1 className="text-xl font-bold  ">문제 1) 상품 목록</h1>
+      <ul className="list-disc mt-6 ml-10">
+        {products.map((product) => (
+          <ProductItem
+            key={product.id}
+            name={product.name}
+            price={product.price}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  const products = [
+    { id: 1, name: '노트북', price: 1200000 },
+    { id: 2, name: '키보드', price: 50000 },
+    { id: 3, name: '마우스', price: 30000 },
+  ];
+  return <ProductList products={products} />;
+}
