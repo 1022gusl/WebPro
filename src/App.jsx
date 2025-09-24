952260dd949bb0233e5fcd25d0e5f7f9b2f42909
1022gusl <1022gusl@gmail.com>
2025-09-24 14:29:06 +0900

    feat: src5-1 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import Header from './components/Header';
-import Home from './components/Home';
-import ProductList from './components/ProductList';
-import Footer from './components/Footer';
+import Datas from './components/Datas';
+import { useState } from 'react';
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  const contentChange = (event) => {
+    setIndex(event.target.value);
+  };
+
   return (
-    <div className="container">
-      <Header />
-      <Home />
-      <ProductList />
-      <Footer />
+    <div className>
+      {Datas.map((data, index) => (
+        <button
+          className="border border-gray-500 mb-4 px-2 bg-gray-100 hover:bg-gray-300 rounded-sm"
+          onClick={contentChange}
+          value={index}
+        >
+          {data.tab} {index}
+        </button>
+      ))}
+      <p>{Datas[index].content}</p>
     </div>
   );
 }
