a9e8c049014d4bb7da717371f0442af2132e396e
1022gusl <1022gusl@gmail.com>
2025-10-28 18:05:03 +0900

    feat:src9-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
-import ProductList from './components/ProductList';
-import Footer from './components/Footer';
+import MyPage from './components/MyPage';
+import Recommend from './components/Recommend';
+import Search from './components/Search';
+import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <Home />
-      <ProductList />
-      <Footer />
+    <div className="App">
+      <Router>
+        <div className="header">
+          <Link to="/" className="item">
+            {' '}
+            Home{' '}
+          </Link>
+          <Link to="/mypage" className="item">
+            {' '}
+            MyPage{' '}
+          </Link>
+          <Link to="/recommend" className="item">
+            {' '}
+            Recommand{' '}
+          </Link>
+          <Link to="/search" className="item">
+            {' '}
+            Search{' '}
+          </Link>
+        </div>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/mypage" Component={MyPage} />
+          <Route path="/recommend" Component={Recommend} />
+          <Route path="/search" Component={Search} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Recommend.js
@@ -0,0 +1,4 @@
+function Recommend() {
+  return <h1>Recommend Page</h1>;
+}
+export default Recommend;
